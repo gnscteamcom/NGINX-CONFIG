0a254212b07459512dba206b0b895221abbac160
Alex Fernando <alex.zonimi@gmail.com>
2020-07-23 17:49:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt -y upgrade
 
 # Install prerequisits
-sudo apt -y install curl gnupg2 ca-certificates lsb-release unzip
+sudo apt -y install curl gnupg2 ca-certificates lsb-release unzip sed
 
 # Install nginx stable version
 echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
@@ -25,5 +25,24 @@ sudo rm phpversion
 # Hide php version
 sudo sed -i "s/expose_php = Off/expose_php = On/g" /etc/php/${PHPV}/fpm/php.ini
 
+# Backup nginx
+cd /etc/nginx
+tar -czvf backup_nginx_$(date +'%F').tar.gz nginx.conf sites-available/ sites-enabled/
 
+# Delete old archives
+rm -r /etc/nginx/sites-available
+em -r /etc/nginx/sites-enabled
+rm /etc/nginx/nginx.conf
 
+# Move new conf
+mv /tmp/NGINX-CONFIG/nginx/* /etc/nginx/
+
+# Generate Diffie-Hellman keys
+openssl dhparam -out /etc/nginx/dhparam.pem 2048
+
+# Install certbot
+sudo apt install certbot
+
+# Create a common ACME-challenge directory (for Let's Encrypt)
+mkdir -p /var/www/_letsencrypt
+chown www-data /var/www/_letsencrypt
