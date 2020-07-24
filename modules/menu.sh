b8069977157d20ceec41d87ab7922926c7f9361c
Alex Fernando <alex.zonimi@gmail.com>
2020-07-23 22:25:39 -0300

    Update menu.sh

--- modules/menu.sh
+++ modules/menu.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
-teste () {
-echo "teste"
+addhost () {
+printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+
+# Question which domain
+read -p "$(tput setaf 6)> $(tput setaf 7)Enter domain: $(tput setaf 6)" domain
+echo -n "$(tput setaf 7)"
+
+# Create website
+cp /etc/nginx/sites-available/default.conf /etc/nginx/sites-available/${domain}.conf
+mkdir -p /var/www/${domain}/public
+chown www-data /var/www/${domain}/public
+
+# Comment out SSL related directives in the configuration
+sed -i -r 's/(listen .*443)/\1;#/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g' /etc/nginx/sites-available/${domain}.conf
+sed -i 's/80/81/g; s/443/80/g' /etc/nginx/sites-available/${domain}.conf
+sudo nginx -t && sudo systemctl reload nginx
+
+# Obtain SSL certificates from Let's Encrypt using Certbot:
+certbot certonly --webroot -d ${domain} --email info@${domain} -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+
+Uncomment SSL related directives in the configuration:
+sed -i -r 's/#?;#//g' /etc/nginx/sites-available/${domain}.conf
+sed -i 's/80/443/g; s/81/80/g' /etc/nginx/sites-available/${domain}.conf
+
+# Configure Certbot to reload NGINX when it successfully renews certificates:
+echo -e '#!/bin/bash\nnginx -t && systemctl reload nginx' | sudo tee /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
+sudo chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
+
+# Add symbolic link
+ln -s /etc/nginx/sites-available/${domain}.conf /etc/nginx/sites-enabled/${domain}.conf
+
+# Reload NGINX to load in your new configuration:
+sudo nginx -t && sudo systemctl reload nginx
 }
 
 language (){
@@ -17,8 +48,8 @@ case "$language" in
     1|01)
     clear
     sleep 0.3
-    teste
-    fun_prog
+    addhost
+    menu
     ;;
     2|02)
     clear
