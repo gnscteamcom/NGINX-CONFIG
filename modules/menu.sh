77d91c4cd0d2c6439083da2ec81bbf2a263d20c8
Alex Fernando <alex.zonimi@gmail.com>
2020-07-26 21:14:15 -0300

    Update menu.sh

--- modules/menu.sh
+++ modules/menu.sh
@@ -245,7 +245,7 @@ echo -ne "    \e[1;3;31m[\e[1;3;36m01\e[1;3;31m] \e[1;3;33mGoogle Drive Advanced
 #    \e[1;3;31m[\e[1;3;36m02\e[1;3;31m] \e[1;3;33mRemove host"
 printf "\n\n"
 echo "$(tput setaf 6)! $(tput setaf 8)Send \"leave\" to return."
-read -p "$(tput setaf 6)> $(tput setaf 7)Select an option \(number\): $(tput setaf 6)" optionmenu
+read -p "$(tput setaf 6)> $(tput setaf 7)Select an option (number): $(tput setaf 6)" optionmenu
 echo -n "$(tput setaf 7)"
 case "$optionmenu" in
     1|01)
@@ -278,11 +278,11 @@ menu (){
 printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 echo -ne "\e[1;3;31m[\e[1;3;32m×\e[1;3;31m] \e[1;3;33mMAIN MENU\e[0m"
 printf "\n\n"
-echo -ne "    \e[1;3;31m[\e[1;3;36m01\e[1;3;31m] \e[1;3;33mAdd host (Domain)    \e[1;3;31m[\e[1;3;36m03\e[1;3;31m] \e[1;3;33mInstall script\'s 
+echo -ne "    \e[1;3;31m[\e[1;3;36m01\e[1;3;31m] \e[1;3;33mAdd host (Domain)    \e[1;3;31m[\e[1;3;36m03\e[1;3;31m] \e[1;3;33mInstall script's 
     \e[1;3;31m[\e[1;3;36m02\e[1;3;31m] \e[1;3;33mRemove host"
 printf "\n\n"
 echo "$(tput setaf 6)! $(tput setaf 8)Send \"leave\" to return."
-read -p "$(tput setaf 6)> $(tput setaf 7)Select an option \(number\): $(tput setaf 6)" optionmenu
+read -p "$(tput setaf 6)> $(tput setaf 7)Select an option (number): $(tput setaf 6)" optionmenu
 echo -n "$(tput setaf 7)"
 case "$optionmenu" in
     1|01)
